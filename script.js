c55c4d9c32e3e79361576739935a15f8f32de353
alisonsilvabt <101824638+alisonsilvabt@users.noreply.github.com>
2023-04-10 22:50:59 -0300

    fix win

--- script.js
+++ script.js
@@ -1,233 +1,153 @@
-window.onload = function () {
+// Defina uma lista de palavras possíveis com suas respectivas dicas
+var palavras = [
+  { palavra: "BOLA", dica: "Objeto esférico usado em diversos esportes", qtnLetras: 4 },
+  { palavra: "CARRO", dica: "Veículo de quatro rodas usado para transporte", qtnLetras: 4 },
+  { palavra: "CASA", dica: "Habitação comum feita de tijolos, madeira, etc.", qtnLetras: 3 },
+  { palavra: "BANANA", dica: "Fruta amarela e doce com formato alongado", qtnLetras: 3 },
+  { palavra: "MESA", dica: "Móvel com tampo plano e suportes para sustentá-lo", qtnLetras: 4 },
+  { palavra: "GATO", dica: "Animal doméstico de estimação com pelos macios", qtnLetras: 4 },
+  { palavra: "ABACAXI", dica: "Fruta tropical com casca dura e polpa doce", qtnLetras: 5 },
+];
+const winSound = new Audio('media/win.wav');
+const endSound = new Audio('media/end.wav');
+const pointSound = new Audio('media/point.wav');
 
-  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  
-  var categories;         // Array of topics
-  var chosenCategory;     // Selected catagory
-  var getHint ;          // Word getHint
-  var word ;              // Selected word
-  var guess ;             // Geuss
-  var geusses = [ ];      // Stored geusses
-  var lives ;             // Lives
-  var counter ;           // Count correct geusses
-  var space;              // Number of spaces in word '-'
 
-  // Get elements
-  var showLives = document.getElementById("mylives");
-  var showCatagory = document.getElementById("scatagory");
-  var getHint = document.getElementById("hint");
-  var showClue = document.getElementById("clue");
+// Inicie o jogo chamando a função para reiniciá-lo
+reiniciarJogo();
 
 
 
-  // create alphabet ul
-  var buttons = function () {
-    myButtons = document.getElementById('buttons');
-    letters = document.createElement('ul');
+// Escolha uma palavra aleatória da lista e exiba sua dica na tela HTML
+var indicePalavraEscolhida = Math.floor(Math.random() * palavras.length);
+var palavraEscolhida = palavras[indicePalavraEscolhida].palavra;
+var letrasPalavraEscolhida = palavras[indicePalavraEscolhida].qtnLetras;
 
-    for (var i = 0; i < alphabet.length; i++) {
-      letters.id = 'alphabet';
-      list = document.createElement('li');
-      list.id = 'letter';
-      list.innerHTML = alphabet[i];
-      check();
-      myButtons.appendChild(letters);
-      letters.appendChild(list);
-    }
-  }
-    
-  
-  // Select Catagory
-  var selectCat = function () {
-    if (chosenCategory === categories[0]) {
-      catagoryName.innerHTML = "The Chosen Category Is Premier League Football Teams";
-    } else if (chosenCategory === categories[1]) {
-      catagoryName.innerHTML = "The Chosen Category Is Films";
-    } else if (chosenCategory === categories[2]) {
-      catagoryName.innerHTML = "The Chosen Category Is Cities";
-    }
-  }
+document.documentElement.style.setProperty('--tamanho-palavra', palavra.length);
+var dicaPalavraEscolhida = palavras[indicePalavraEscolhida].dica;
+document.getElementById("dica").innerHTML = "Dica: " + dicaPalavraEscolhida;
 
-  // Create geusses ul
-   result = function () {
-    wordHolder = document.getElementById('hold');
-    correct = document.createElement('ul');
-
-    for (var i = 0; i < word.length; i++) {
-      correct.setAttribute('id', 'my-word');
-      guess = document.createElement('li');
-      guess.setAttribute('class', 'guess');
-      if (word[i] === "-") {
-        guess.innerHTML = "-";
-        space = 1;
+// Defina uma função para criar uma árvore binária a partir da palavra escolhida
+function criarArvore(palavra) {
+  var letras = palavra.split("");
+  var raiz = { letra: letras[0], esquerda: null, direita: null };
+  var atual = raiz;
+  for (var i = 1; i < letras.length; i++) {
+      if (Math.random() < 0.5) {
+          atual.esquerda = { letra: letras[i], esquerda: null, direita: null };
+          atual = atual.esquerda;
       } else {
-        guess.innerHTML = "_";
+          atual.direita = { letra: letras[i], esquerda: null, direita: null };
+          atual = atual.direita;
       }
-
-      geusses.push(guess);
-      wordHolder.appendChild(correct);
-      correct.appendChild(guess);
-    }
   }
-  
-  // Show lives
-   comments = function () {
-    showLives.innerHTML = "You have " + lives + " lives";
-    if (lives < 1) {
-      showLives.innerHTML = "Game Over";
-    }
-    for (var i = 0; i < geusses.length; i++) {
-      if (counter + space === geusses.length) {
-        showLives.innerHTML = "You Win!";
-      }
-    }
-  }
-
-      // Animate man
-  var animate = function () {
-    var drawMe = lives ;
-    drawArray[drawMe]();
-  }
-
-  
-   // Hangman
-  canvas =  function(){
-
-    myStickman = document.getElementById("stickman");
-    context = myStickman.getContext('2d');
-    context.beginPath();
-    context.strokeStyle = "#fff";
-    context.lineWidth = 2;
-  };
-  
-    head = function(){
-      myStickman = document.getElementById("stickman");
-      context = myStickman.getContext('2d');
-      context.beginPath();
-      context.arc(60, 25, 10, 0, Math.PI*2, true);
-      context.stroke();
-    }
-    
-  draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
-    
-    context.moveTo($pathFromx, $pathFromy);
-    context.lineTo($pathTox, $pathToy);
-    context.stroke(); 
+  return raiz;
 }
 
-   frame1 = function() {
-     draw (0, 150, 150, 150);
-   };
-   
-   frame2 = function() {
-     draw (10, 0, 10, 600);
-   };
-  
-   frame3 = function() {
-     draw (0, 5, 70, 5);
-   };
-  
-   frame4 = function() {
-     draw (60, 5, 60, 15);
-   };
-  
-   torso = function() {
-     draw (60, 36, 60, 70);
-   };
-  
-   rightArm = function() {
-     draw (60, 46, 100, 50);
-   };
-  
-   leftArm = function() {
-     draw (60, 46, 20, 50);
-   };
-  
-   rightLeg = function() {
-     draw (60, 70, 100, 100);
-   };
-  
-   leftLeg = function() {
-     draw (60, 70, 20, 100);
-   };
-  
-  drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1]; 
+// Crie a árvore binária para a palavra escolhida
+var arvore = criarArvore(palavraEscolhida);
 
+// Defina uma função para verificar se uma letra está na árvore binária
+function verificarLetra(letra, no) {
+  if (no === null) {
+      return false;
+  }
+  if (no.letra === letra) {
+      return true;
+  }
+  return verificarLetra(letra, no.esquerda) || verificarLetra(letra, no.direita);
+}
 
-  // OnClick Function
-   check = function () {
-    list.onclick = function () {
-      var geuss = (this.innerHTML);
-      this.setAttribute("class", "active");
-      this.onclick = null;
-      for (var i = 0; i < word.length; i++) {
-        if (word[i] === geuss) {
-          geusses[i].innerHTML = geuss;
-          counter += 1;
-        } 
-      }
-      var j = (word.indexOf(geuss));
-      if (j === -1) {
-        lives -= 1;
-        comments();
-        animate();
+// Defina uma função para atualizar a exibição da palavra
+function atualizarPalavra(letrasCorretas) {
+  var palavraExibicao = "";
+  for (var i = 0; i < palavraEscolhida.length; i++) {
+      if (letrasCorretas.indexOf(palavraEscolhida[i]) !== -1) {
+          palavraExibicao += palavraEscolhida[i] + " ";
       } else {
-        comments();
+          palavraExibicao += "_ ";
       }
-    }
   }
-  
-    
-  // Play
-  play = function () {
-    categories = [
-        ["everton", "liverpool", "swansea", "chelsea", "hull", "manchester-city", "newcastle-united"],
-        ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
-        ["manchester", "milan", "madrid", "amsterdam", "prague"]
-    ];
+  document.getElementById("palavra").innerHTML = palavraExibicao
+}
 
-    chosenCategory = categories[Math.floor(Math.random() * categories.length)];
-    word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
-    word = word.replace(/\s/g, "-");
-    console.log(word);
-    buttons();
-
-    geusses = [ ];
-    lives = 10;
-    counter = 0;
-    space = 0;
-    result();
-    comments();
-    selectCat();
-    canvas();
+// Defina uma função para verificar se a palavra foi adivinhada
+function verificarAdivinhacao() {
+  var letra = document.getElementById("letra").value.toUpperCase();
+  document.getElementById("letra").value = "";
+  if (verificarLetra(letra, arvore)) {
+      letrasCorretas.push(letra);
+      pointSound.play();
+  } else if (letra === palavraEscolhida) {
+      letrasCorretas = palavraEscolhida.split("");
+      winSound.play();
+      alert("Parabéns, você acertou a palavra " + palavraEscolhida + "!");
+      reiniciarJogo();
   }
-
-  play();
+  else {
+      tentativasRestantes--;
+      atualizarBoneco();
+  }
+  atualizarPalavra(letrasCorretas);
+  atualizarTentativas();
+  if (tentativasRestantes === 0) {
+      endSound.play();
+      alert("Fim de jogo! A palavra era " + palavraEscolhida);
+      reiniciarJogo();
+  } else if (letrasCorretas.length === letrasPalavraEscolhida) {
+      winSound.play();
+      alert("Parabéns, você acertou a palavra " + palavraEscolhida + "!");
+      reiniciarJogo();
+  } else {
+      console.log(" ---------------------------- ")
+      console.log("letrasCorretas: " +letrasCorretas)
+      console.log("letrasCorretasLength: " +letrasCorretas.length)
+      console.log("PalavraEscolhida: " + palavraEscolhida)
+      console.log("PalavraEscolhidaLength: " + palavraEscolhida.length)
+  }
+}
   
-  // Hint
+  // Defina uma função para atualizar a exibição do número de tentativas restantes
+function atualizarTentativas() {
+  var tentativasExibicao = "Tentativas restantes: " + tentativasRestantes;
+  document.getElementById("tentativas").innerHTML = tentativasExibicao;
+}
+  
+  // Defina uma função para atualizar a exibição do boneco
+function atualizarBoneco() {
+  var partesBoneco = [
+  "cabeca", "corpo", "braco-esquerdo", "braco-direito",
+  "perna-esquerda", "perna-direita"
+  ];
+  var parteAtual = partesBoneco[5 - tentativasRestantes];
+  document.getElementById(parteAtual).style.display = "block";
+}
+  
+  // Defina uma função para reiniciar o jogo
+function reiniciarJogo() {
+  // Escolha uma nova palavra aleatória da lista e exiba sua dica na tela HTML
+  indicePalavraEscolhida = Math.floor(Math.random() * palavras.length);
+  palavraEscolhida = palavras[indicePalavraEscolhida].palavra;
+  dicaPalavraEscolhida = palavras[indicePalavraEscolhida].dica;
+  document.getElementById("dica").innerHTML = "Dica: " + dicaPalavraEscolhida;
+  // Crie uma nova árvore binária para a nova palavra escolhida
+  arvore = criarArvore(palavraEscolhida);
 
-    hint.onclick = function() {
+  // Reinicie as variáveis do jogo
+  letrasCorretas = [];
+  tentativasRestantes = 6;
+  atualizarPalavra(letrasCorretas);
+  atualizarTentativas();
+  reiniciarBoneco();
+}
 
-      hints = [
-        ["Based in Mersyside", "Based in Mersyside", "First Welsh team to reach the Premier Leauge", "Owned by A russian Billionaire", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"],
-        ["Science-Fiction horror film", "1971 American action film", "Historical drama", "Anamated Fish", "Giant great white shark"],
-        ["Northern city in the UK", "Home of AC and Inter", "Spanish capital", "Netherlands capital", "Czech Republic capital"]
-    ];
-
-    var catagoryIndex = categories.indexOf(chosenCategory);
-    var hintIndex = chosenCategory.indexOf(word);
-    showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
-  };
-
-   // Reset
-
-  document.getElementById('reset').onclick = function() {
-    correct.parentNode.removeChild(correct);
-    letters.parentNode.removeChild(letters);
-    showClue.innerHTML = "";
-    context.clearRect(0, 0, 400, 400);
-    play();
+// Defina uma função para reiniciar a exibição do boneco
+function reiniciarBoneco() {
+  var partesBoneco = [
+  "cabeca", "braco-esquerdo", "braco-direito",
+  "perna-esquerda", "perna-direita"
+  ];
+  for (var i = 0; i < partesBoneco.length; i++) {
+      document.getElementById(partesBoneco[i]).style.display = "none";
   }
 }
